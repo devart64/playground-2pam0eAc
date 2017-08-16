f2c9ca0618065ad9c0c3c4557dac2b2fb87a04e1
Stephen Dupré <stephendupre64@gmail.com>
2017-08-16 09:17:46 +0200

    Update Universe.php

--- php-template/Universe.php
+++ php-template/Universe.php
@@ -2,7 +2,7 @@
     function countAllStars($galaxies) {
         $totalStars = 0;
         foreach ($galaxies as $star) {
-            $totalStars = $star; //Hint: Change = to +=
+            $totalStars += $star; //Hint: Change = to +=
         }
         return $totalStars;
     }
